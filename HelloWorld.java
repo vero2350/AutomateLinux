e6edbe45b2e8de00872f6a24561587e23b98d86e
“vero2350” <vskleon@gmail.com>
2020-09-25 15:31:14 +0000

    added // to

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,12 @@
 //this is intial webpage 
 
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello, World");
+
+	// this code will print
+	System.out.println("Welcome to Git newday");
     }
 
-
+}
